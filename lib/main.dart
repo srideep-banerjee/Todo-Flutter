ce945569f99b605b101b7bc6170536be221509dc
srideep-banerjee <banerjee.srideep@gmail.com>
2024-07-10 10:49:00 +0530

    Added localstorage initializer

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:todo/main_screen.dart';
+import 'package:localstorage/localstorage.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initLocalStorage();
   runApp(const MyApp());
 }
 
